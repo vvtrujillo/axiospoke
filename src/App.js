b99d6ab0f3eec7a6394bbdb2c2523ee62e4e4a66
vvtrujillo <victor.valenzuela.t@gmail.com>
2022-08-16 14:54:53 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(()=> {   
+
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+      .then(resp => {
+        setData(resp.data.results);        
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon</h1>
+      {data.map((dato) => 
+        <ul>          
+          <li>{dato.name}</li>
+          <li>{dato.url}</li>
+        </ul>)
+      }            
     </div>
   );
 }
